6201a87197144cdb1ec48f666a42929039bcfce3
Warrenn Enslin <warrennenslin@Warrenns-MacBook-Pro.local>
2019-06-21 19:20:33 +0200

    added fieldglass time

--- auto-web/tests/unit/test-handler.js
+++ auto-web/tests/unit/test-handler.js
@@ -4,9 +4,10 @@ const app = require('../../app.js');
 const chai = require('chai');
 const expect = chai.expect;
 var event, context;
+process.env.Mocha = true;
 
-describe('Tests index', function () {
-    it('verifies successful response', async () => {
+describe('Tests index', function() {
+    it('verifies successful response', async() => {
         const result = await app.lambdaHandler(event, context)
 
         expect(result).to.be.an('object');
